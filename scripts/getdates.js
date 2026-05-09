65267ebb88f60c8b7b057e2ada4d48b236a594e5
dember-byu <dember93hide@gmail.com>
2026-05-09 10:56:13 -0400

    made it

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,6 +1,5 @@
-// Obtener el año actual para el copyright
+
 const currentYear = new Date().getFullYear();
 document.getElementById("currentyear").textContent = currentYear;
 
-// Obtener la fecha de última modificación
 document.getElementById("lastModified").textContent = `Last Modification: ${document.lastModified}`;
